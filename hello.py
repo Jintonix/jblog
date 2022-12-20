f0b4ae44cfcc7782b8776db8941e16c9da036d09
Jintonix <jintoschneider@gmail.com>
2022-12-20 20:52:27 +0100

    added WTForms, Messages as well as Image, CSS and JS folders

--- hello.py
+++ hello.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+
+# Import stuff for WTF
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask instance, this is how you always start a flask porject
 app = Flask(__name__)
+
+# Create placeholder CSRF Token
+app.config['SECRET_KEY'] = '123456'
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # create a route
 @app.route("/")
@@ -33,3 +46,18 @@ def page_not_found(e):
 
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Create Name page
+@app.route("/name", methods=["GET","POST"])
+def name():
+    name = None # Required because the first time you open the page the Field won't be filled
+    form = NamerForm()
+    #validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+        flash("Form submitted successfully!")
+        
+    return render_template("name.html", 
+        name = name,
+        form = form)
